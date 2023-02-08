2de32709ab6b49752f7fe84e7b4021130c66b883
JRisJunior <jeffmrubin@gmail.com>
2023-02-08 13:22:50 -0600

    add jb gem and views to send custom headers to frontend

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,30 +1,30 @@
 class EventsController < ApplicationController
 
   def index
-    events = Event.all
-    render json: events.as_json
+    @events = Event.all
+    render template: "events/index"
   end
 
   def show
-    event = Event.find_by(id: params[:id])
-    render json: event.as_json
+    @event = Event.find_by(id: params[:id])
+    render json: @event.as_json
   end
 
   def create
-    event = Event.new(
+    @event = Event.new(
       subject: params[:subject],
       description: params[:description],
       user_id: 1,
       start_time: params[:start_time],
       end_time: params[:end_time]
     )
-    event.save
-    render json: event.as_json
+    @event.save
+    render json: @event.as_json
   end
 
   def destroy
-    event = Event.find_by(id: params[:id])
-    event.delete
+    @event = Event.find_by(id: params[:id])
+    @event.delete
     render json: {message: "this has been deleted"}
   end
 
